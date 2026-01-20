9724e63e11c9fae9e6f55964da01bd658ca9ad32
Sebsatian Iguavita Roa <sebastianiguavitaroa@outlook.com>
2026-01-20 15:34:50 -0500

    feat: add linter and fix on unit-test wf

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 - Caching: when you want to reuse files that do not change often between jobs and workflows runs such as build dependencies from package management systems  like npm, gradle or maven, etc
 - Artifacts: when you want to save files produced by a job to view after a workflow run has eneded such as some build boundaries, test results, etc.
+
+## Add Linter
+
+- npm i eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin jiti -D
+- npx eslint --init this genarates "eslint.config"
+- npx eslint
---
+++ eslint.config.ts
@@ -0,0 +1,54 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["node_modules/**", "coverage/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        before: "readonly",
+        beforeEach: "readonly",
+        after: "readonly",
+        afterEach: "readonly",
+        context: "readonly",
+        specify: "readonly",
+        module: "readonly",
+        require: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-prototype-builtins": "off",
+      "max-lines-per-function": ["warn", 50],
+      "max-len": ["warn", { code: 150 }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { 
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_"
+        }
+      ],
+      "@typescript-eslint/explicit-function-return-type": "warn",
+    },
+  },
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    rules: {
+      "max-lines-per-function": "off",
+    },
+  },
+];
